cf42f56ab210c4311e4a255c286c6cea4ae0c2f6
Ravindran <krvravindran@gmail.com>
2020-07-06 11:52:48 +0530

    update url

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,6 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 $route['default_controller'] = 'home';
 
+
 // Load default conrtoller when have only currency from multilanguage
 $route['^(\w{2})$'] = $route['default_controller'];
 
